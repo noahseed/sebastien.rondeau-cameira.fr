9eb5bcadc430f2aaeb549aa69bc59c533f260641
Sébastien RONDEAU-CAMEIRA <2760742+noahseed@users.noreply.github.com>
2019-02-10 19:27:51 +0100

    Initial commit

--- public_html/includes/inc_header.php
+++ public_html/includes/inc_header.php
@@ -1,14 +1,14 @@
-    <body>
-        <header>
-            <a href="/">
-                <img src="/images/aum_blanc.png" alt="Logo" id="logo-img">
-                <h1 id="logo-txt">sebastien.rondeau-cameira.fr</h1>
-            </a>
-            <div id="barre-lang">
-                <form method="POST">
-                    <input type="submit" name="lang" id="flag-fr" value="fr"> -
-                    <input type="submit" name="lang" id="flag-en" value="en"> -
-                    <input type="submit" name="lang" id="flag-pt" value="pt">
-                </form>
-            </div>
-        </header>
+<body>
+    <header>
+        <a href="/">
+            <img src="/images/aum_blanc.png" alt="Logo" id="logo-img">
+            <h1 id="logo-txt">sebastien.rondeau-cameira.fr</h1>
+        </a>
+        <div id="barre-lang">
+            <form method="POST">
+                <input type="submit" name="lang" id="flag-fr" value="fr"> -
+                <input type="submit" name="lang" id="flag-en" value="en"> -
+                <input type="submit" name="lang" id="flag-pt" value="pt">
+            </form>
+        </div>
+    </header>

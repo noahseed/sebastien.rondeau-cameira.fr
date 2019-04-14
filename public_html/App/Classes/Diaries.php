8d273d264f8b32a67e943cee094e67bce0fdb9b6
Sébastien RONDEAU CAMEIRA <2760742+noahseed@users.noreply.github.com>
2019-04-14 16:04:41 +0200

    Update Diaries.php

--- public_html/App/Classes/Diaries.php
+++ public_html/App/Classes/Diaries.php
@@ -2,6 +2,79 @@
 
 class Diaries extends Db
 {
+    public function add(Diary $diary) : int
+    {
+        $sql = "INSERT INTO
+        `diary`
+        (
+            `diary_date`,
+            `diary_slug`,
+            `diary_title`,
+            `diary_content`
+        )
+        VALUES
+        (
+            NOW(),
+            :diary_slug,
+            :diary_title,
+            :diary_content
+        )";
+
+        $stmt = $this->pdo->prepare($sql);
+
+        $stmt->bindValue(':diary_slug', $diary->diary_slug);
+        $stmt->bindValue(':diary_title', $diary->diary_title);
+        $stmt->bindValue(':diary_content', $diary->diary_content);
+
+        $result = 0;
+
+        if($stmt->execute())
+        {
+            $result = $stmt->rowCount();
+        }
+
+        $stmt->closeCursor();
+
+        return $result;
+    }
+
+    public function delete(int $diary_id)
+    {
+        $sql = "DELETE FROM
+        `diary`
+        WHERE
+        `diary_id` = :diary_id";
+
+        $stmt = $this->pdo->prepare($sql);
+
+        $stmt->bindValue(':diary_id', $diary_id, PDO::PARAM_INT);
+
+        $stmt->execute();
+
+        $stmt->closeCursor();
+    }
+
+    public function edit(Diary $diary)
+    {
+        $sql = "UPDATE
+        `diary`
+        SET
+        `diary_title` = :diary_title,
+        `diary_content` = :diary_content
+        WHERE
+        `diary_id` = :diary_id";
+
+        $stmt = $this->pdo->prepare($sql);
+
+        $stmt->bindValue(':diary_id', $diary->diary_id, PDO::PARAM_INT);
+        $stmt->bindValue(':diary_title', $diary->diary_title);
+        $stmt->bindValue(':diary_content', $diary->diary_content);
+
+        $stmt->execute();
+
+        $stmt->closeCursor();
+    }
+
     public function find(int $diary_id)
     {
         $sql = "SELECT
@@ -46,40 +119,6 @@ class Diaries extends Db
         if($stmt->execute())
         {
             $result = $stmt->fetchAll();
-        }
-
-        $stmt->closeCursor();
-
-        return $result;
-    }
-
-    public function add(Diary $diary) : int
-    {
-        $sql = "INSERT INTO
-        `diary`
-        (
-            `diary_id`,
-            `diary_date`,
-            `diary_slug`,
-            `diary_title`,
-            `diary_content`
-        )
-        VALUES
-        (:diary_id, :diary_date, :diary_slug, :diary_title, :diary_content)";
-
-        $stmt = $this->pdo->prepare($sql);
-
-        $stmt->bindValue(':diary_id', strip_tags($diary->diary_id));
-        $stmt->bindValue(':diary_date', strip_tags($diary->diary_date));
-        $stmt->bindValue(':diary_slug', strip_tags($diary->diary_slug));
-        $stmt->bindValue(':diary_title', strip_tags($diary->diary_title));
-        $stmt->bindValue(':diary_content', strip_tags($diary->diary_content));
-
-        $result = 0;
-
-        if($stmt->execute())
-        {
-            $result = $stmt->rowCount();
         }
 
         $stmt->closeCursor();

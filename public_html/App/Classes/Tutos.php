4aa72eb667a8ca72e7081fd2b21bd28bb089ae21
Sébastien RONDEAU-CAMEIRA <2760742+noahseed@users.noreply.github.com>
2019-04-14 13:21:36 +0200

    Update Tutos.php

--- public_html/App/Classes/Tutos.php
+++ public_html/App/Classes/Tutos.php
@@ -2,12 +2,82 @@
 
 class Tutos extends Db
 {
-    public function find(int $id)
+    public function add(Tuto $tuto)
+    {
+        $sql = "INSERT INTO
+        `tutos`
+        (
+            `tuto_date`,
+            `tuto_title`,
+            `tuto_content`
+        )
+        VALUES
+        (
+            NOW(),
+            :tuto_title,
+            :tuto_content
+        )";
+
+        $stmt = $this->pdo->prepare($sql);
+
+        $stmt->bindValue(':tuto_title', $tuto->tuto_title);
+        $stmt->bindValue(':tuto_content', $tuto->tuto_content);
+
+        $result = 0;
+
+        if($stmt->execute())
+        {
+            $result = $stmt->rowCount();
+        }
+
+        $stmt->closeCursor();
+
+        return $result;
+    }
+
+    public function delete(int $tuto_id)
+    {
+        $sql = "DELETE FROM
+        `tutos`
+        WHERE
+        `tuto_id` = :tuto_id";
+
+        $stmt = $this->pdo->prepare($sql);
+
+        $stmt->bindValue(':tuto_id', $tuto_id, PDO::PARAM_INT);
+
+        $stmt->execute();
+
+        $stmt->closeCursor();
+    }
+
+    public function edit(Tuto $tuto)
+    {
+        $sql = "UPDATE
+        `tutos`
+        SET
+        `tuto_title` = :tuto_title,
+        `tuto_content` = :tuto_content
+        WHERE
+        `tuto_id` = :tuto_id";
+
+        $stmt = $this->pdo->prepare($sql);
+
+        $stmt->bindValue(':tuto_id', $tuto->tuto_id, PDO::PARAM_INT);
+        $stmt->bindValue(':tuto_title', $tuto->tuto_title);
+        $stmt->bindValue(':tuto_content', $tuto->tuto_content);
+
+        $stmt->execute();
+
+        $stmt->closeCursor();
+    }
+
+    public function find(int $tuto_id)
     {
         $sql = "SELECT
         `tuto_id`,
-        `tuto_title`,
         `tuto_date`,
+        `tuto_title`,
         `tuto_content`
         FROM `tutos`
         WHERE `tuto_id` = :tuto_id";
@@ -16,7 +86,7 @@ class Tutos extends Db
 
         $stmt->bindValue(
             ':tuto_id',
-            $id,
+            $tuto_id,
             PDO::PARAM_INT
         );
 
@@ -34,8 +104,8 @@ class Tutos extends Db
     {
         $sql = "SELECT
         `tuto_id`,
-        `tuto_title`,
         `tuto_date`,
+        `tuto_title`,
         `tuto_content`
         FROM `tutos`";
 

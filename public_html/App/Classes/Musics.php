f52adc5880dcc828fdeee6e6e1e605fe1b174c35
Sébastien RONDEAU-CAMEIRA <2760742+noahseed@users.noreply.github.com>
2019-04-14 13:21:30 +0200

    Update Musics.php

--- public_html/App/Classes/Musics.php
+++ public_html/App/Classes/Musics.php
@@ -2,6 +2,76 @@
 
 class Musics extends Db
 {
+    public function add(Music $music)
+    {
+        $sql = "INSERT INTO
+        `music`
+        (
+            `music_date`,
+            `music_title`,
+            `music_content`
+        )
+        VALUES
+        (
+            NOW(),
+            :music_title,
+            :music_content
+        )";
+
+        $stmt = $this->pdo->prepare($sql);
+
+        $stmt->bindValue(':music_title', $music->music_title);
+        $stmt->bindValue(':music_content', $music->music_content);
+
+        $result = 0;
+
+        if($stmt->execute())
+        {
+            $result = $stmt->rowCount();
+        }
+
+        $stmt->closeCursor();
+
+        return $result;
+    }
+
+    public function delete(int $music_id)
+    {
+        $sql = "DELETE FROM
+        `music`
+        WHERE
+        `music_id` = :music_id";
+
+        $stmt = $this->pdo->prepare($sql);
+
+        $stmt->bindValue(':music_id', $music_id, PDO::PARAM_INT);
+
+        $stmt->execute();
+
+        $stmt->closeCursor();
+    }
+
+    public function edit(Music $music)
+    {
+        $sql = "UPDATE
+        `music`
+        SET
+        `music_title` = :music_title,
+        `music_content` = :music_content
+        WHERE
+        `music_id` = :music_id";
+
+        $stmt = $this->pdo->prepare($sql);
+
+        $stmt->bindValue(':music_id', $music->music_id, PDO::PARAM_INT);
+        $stmt->bindValue(':music_title', $music->music_title);
+        $stmt->bindValue(':music_content', $music->music_content);
+
+        $stmt->execute();
+
+        $stmt->closeCursor();
+    }
+
     public function find(int $id)
     {
         $sql = "SELECT

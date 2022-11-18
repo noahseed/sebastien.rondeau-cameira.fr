8c219936cdf311e647ab21e7a9086208dfe3b5a3
Sébastien <2760742+noahseed@users.noreply.github.com>
2022-11-18 13:34:44 +0100

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -2,7 +2,7 @@
 <html lang="fr">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Sébastien RONDEAU-CAMEIRA</title>
         <meta name="robots" content="index,follow">
         <meta name="Rating" content="General">

b9e934b8c5880ceb6c4ca6fef4f8d7171aa99f1b
Sébastien <2760742+noahseed@users.noreply.github.com>
2022-11-25 22:36:39 +0100

    Update Classes.php

--- public_html/App/Classes.php
+++ public_html/App/Classes.php
@@ -6,8 +6,8 @@
 //require 'Classes/Admin.php';
 //require 'Classes/Admins.php';
 
-require 'Classes/Group.php';
-require 'Classes/Groups.php';
+//require 'Classes/Group.php';
+//require 'Classes/Groups.php';
 
 //require 'Classes/Page.php';
 //require 'Classes/Pages.php';
